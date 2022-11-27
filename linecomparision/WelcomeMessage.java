d8eac420875479ed544da07da32b7b766d3c601e
vp22j <vp22jnp@gmail.com>
2022-11-27 22:51:28 +0530

    added:welcome message

---
+++ linecomparision/WelcomeMessage.java
@@ -0,0 +1,7 @@
+package com.linecomparision;
+
+public class WelcomeMessage {
+    public static void main(String[] args) {
+        System.out.println("welcome to line comparsion program");
+    }
+}
